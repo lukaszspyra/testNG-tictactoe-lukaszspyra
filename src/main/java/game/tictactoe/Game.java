89a34f1f8dda8927cdb7daa995c61d315b659b79
lukaszspyra <lukasz.spyrka@gmail.com>
2020-12-09 12:57:40 +0100

    Implement printBoard() in UI

--- src/main/java/game/tictactoe/Game.java
+++ src/main/java/game/tictactoe/Game.java
@@ -3,22 +3,24 @@ package game.tictactoe;
 public final class Game {
 
     private final Arbiter arbiter;
+    private final UI ui;
     private Board board;
     private Player currentPlayer;
 
-    public Game(final Board board, final Arbiter arbiter, final Player firstPlayer) {
+
+    public Game(final Board board, final UI ui, final Arbiter arbiter, final Player firstPlayer) {
         this.board = board;
+        this.ui = ui;
         this.arbiter = arbiter;
         this.currentPlayer = firstPlayer;
     }
 
     public Player play() {
-        Move move;
 
         do {
-            move = currentPlayer.makeMove(board);
+            Move move = currentPlayer.makeMove(board);
             board = board.playAt(move);
-            System.out.println(board);
+            ui.printBoard(board);
             currentPlayer = currentPlayer.nextPlayer();
         } while (!arbiter.judge(board).endsGame());
 
--- src/main/java/game/tictactoe/UI.java
+++ src/main/java/game/tictactoe/UI.java
@@ -44,4 +44,35 @@ public final class UI {
         System.out.println(userPrompt);
     }
 
+
+    public void printBoard(final Board board) {
+        String upperDelimiter = "-";
+        String sideDelimiter = " | ";
+        System.out.println("      1   " + "  2   " + "  3   ");
+        System.out.println("   " + upperDelimiter.repeat(19));
+        int columnNumber = 0;
+        for (var rows : board.getGameBoard()) {
+            columnNumber++;
+            System.out.print(columnNumber + " ");
+            System.out.print(sideDelimiter);
+            for (int i = 0; i < rows.length; i++) {
+                printCell(rows[i]);
+                System.out.print(sideDelimiter);
+            }
+            System.out.println();
+            System.out.println("   " + upperDelimiter.repeat(19));
+        }
+        System.out.println();
+    }
+
+    private void printCell(Player player) {
+        if (player == null) {
+            System.out.print("   ");
+            return;
+        }
+        switch (player) {
+            case X -> System.out.print(" X ");
+            case O -> System.out.print(" O ");
+        }
+    }
 }
